fa3ec47f61bf12692515adc7df7d188fb466362a
XabierSoro <125381291+XabierSoro@users.noreply.github.com>
2023-02-14 17:35:39 +0100

    Funcion_range

---
+++ range.py
@@ -0,0 +1,8 @@
+for i in range(10): #Repite el print 10 veces
+    print(i)
+for j in range(2, 20):
+    print(j)
+for x in range(10, 100, 5):# Repite el print de 10 a 100 de 5 en 5
+    print(x)
+for v in range(5):# Repite XABI 5 veces
+    print("XABI")
