d37cb254036f7e0f304528fe61d3de12daa1060e
XabierSoro <125381291+XabierSoro@users.noreply.github.com>
2023-02-20 16:27:44 +0100

    Add files via upload

---
+++ while.py
@@ -0,0 +1,8 @@
+i = 10
+while i > 0:
+    if i ==5:
+        print("i es 5")
+        break #Para el bucle cuando la i llegue a 5, saldrá del bucle
+    print("Hola" + str(i))
+    i=i-1
+print("fin del bucle")
