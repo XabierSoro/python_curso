1a761a2c07bfb991f9e61d314b48df7118acf0c8
XabierSoro <125381291+XabierSoro@users.noreply.github.com>
2023-02-20 17:25:10 +0100

    bucle while

---
+++ retobanco.py
@@ -0,0 +1,15 @@
+accion = str(input("¿Tienes alguna transacción para introducir? (s/n)"))
+count=0
+total=0
+count=0
+cantidades=[] # Creamos la lista de cantidades
+while accion == "s":
+    cantidad = float(input("Introduzca cantidad "))#nos pide la cantidad de la transacción
+    cantidades.append(cantidad)# introducimos la cntidad en la lista de cantidades
+    count=count+1 # Ponemos contador para conocer el numero de transacciones hechas y así poder hacer la media
+    total=cantidad + total # Vamos sumando las transacciones
+    accion = str(input("¿Tienes alguna transacción para introducir? (s/n)"))# volvemos a preguntar para salir o no del while
+media=total/count
+print(f"las transacciones realizadas son las siguientes {cantidades}")
+print(f"La suma total de las transacciones es {total}" )
+print(f"La media de las transacciones es {media:.2f} ")
