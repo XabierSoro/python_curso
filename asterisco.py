35f50c84ce802a1cb666ba792d7806184734c2ff
XabierSoro <125381291+XabierSoro@users.noreply.github.com>
2023-02-21 16:36:59 +0100

    ejercicio asterísco

---
+++ asterisco.py
@@ -0,0 +1,12 @@
+i=0
+ch ="*"
+while i < 10:
+    print(ch*i)# Multiplica * por i, si i es 2 el resultado será **
+    i=i+2
+else:
+    print("........")
+
+i=9
+while i > 0:
+    print(ch*i)
+    i=i-2
