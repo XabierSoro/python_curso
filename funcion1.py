bb538f3a0786ed331a1173397308807887ed9f8b
XabierSoro <125381291+XabierSoro@users.noreply.github.com>
2023-02-23 16:40:38 +0100

    Add files via upload

---
+++ funcion1.py
@@ -0,0 +1,15 @@
+def sumar(a,b): #definimos la función con los parámetros que vamos a utilizar
+    return a+b
+# hasta aquí la función
+
+x=sumar (50,25)# llamo a la función sumar
+print(x)
+#####################################
+
+def restar(a,b):
+    return a-b # nos devuelve el resultado
+# Esta es la función restar
+
+v=restar (5,20)# llamo a la función restar
+print(v)
+
