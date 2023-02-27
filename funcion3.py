066425849efd52986a2f56759902f6489ec2b43c
XabierSoro <125381291+XabierSoro@users.noreply.github.com>
2023-02-27 17:57:07 +0100

    Add files via upload

---
+++ funcion3.py
@@ -0,0 +1,10 @@
+def hola():
+    nombre = input("Cual es tu nombre? ")
+    print(f"Bienvenido {nombre}")
+
+
+
+if __name__ =="__main__":
+    for i in range(3):# para que nos repita tres veces la función
+        hola()
+
